4525e1dcd894e8a473d7e2d07c81756196a32036
xiarui <28723656@qq.com>
2019-06-03 00:08:36 +0800

    update:修改了加密方式（加盐的方式）

--- src/main/java/com/java/test01/utils/Md5Util.java
+++ src/main/java/com/java/test01/utils/Md5Util.java
@@ -2,13 +2,20 @@ package com.java.test01.utils;
 
 import org.springframework.util.DigestUtils;
 
+import java.util.Random;
+
 public class Md5Util {
 
     public static String salt ="lostwind";
 
     // 有盐
     public static String getMD5WithSalt(String str){
-        String base = str+salt;
+        //原来的加盐的方式
+       // String base = str+salt;
+        // 现在的加盐方式
+        Random random = new Random();
+        // 0<=x <= 9999
+        String base = str+random.nextInt(10000);
         return DigestUtils.md5DigestAsHex(base.getBytes());
     }
 
